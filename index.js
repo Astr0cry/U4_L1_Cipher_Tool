610a3f4c22cc0fb3aa87b8fa47f7d42d3f9ac903
Maeson <maesoneser1219@gmail.com>
2026-02-19 10:18:04 -0500

    finished

--- index.js
+++ index.js
@@ -2,13 +2,25 @@
 function encrypt(string,shift){
     let charCodes = [];
     for(let i=0; i < string.length;i++){
-        const code = string.charCodeAt(i) +shift;
-        charCodes.push(Number(code));
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        const lower = "abcdefghijklmnopqrstuvwxyz"
+        if(upper.includes(string[i])){
+            const upperIndex = (upper.indexOf(string[i])+Number(shift)+26) % 26;
+            const code = upper.charCodeAt(upperIndex);
+            charCodes.push(code);
+        }
+        else if(lower.includes(string[i])){
+            const lowerIndex = (lower.indexOf(string[i])+Number(shift)+26)%26;
+            const code = lower.charCodeAt(lowerIndex);
+            charCodes.push(code);
+        }
+        else{
+            charCodes.push(string.charCodeAt(i));
+        }
     }
     let encryptedString = ""
     for(let i=0; i<charCodes.length;i++){
-        const char = String.fromCharCode[charCodes[i]];
-        console.log(String.fromCharCode[charCodes]);
+        let char = String.fromCharCode(charCodes[i]);
         encryptedString+=char;
     }
     return encryptedString
@@ -19,12 +31,21 @@ function encrypt(string,shift){
 function updateOutput(){
     const mode = document.getElementById("mode");
     const input = document.getElementById("input");
-    const shift = document.getElementById("shift");
-    const output = document.getElementById("output");
-
+    const shift = document.getElementById("shifts");
+    if(shift.value>26){
+        shift.value = 26;
+    }
+    else if(shift.value<0){
+        shift.value=0;
+    }
     if(mode.value=="Encrypt"){
-        encrypt(input.value,shift.value);
+        const output = document.getElementById("output");
+        const outputString = encrypt(input.value,shift.value);
+        output.value = outputString;
+    }
+    else{
+        const output = document.getElementById("output");
+        const outputString = encrypt(input.value,-shift.value);
+        output.value = outputString;
     }
 }
-
-console.log(encrypt("hello World",1));
